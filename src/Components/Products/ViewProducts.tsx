a4c2ca28554d865bbf8050aa67ed3cbd8bd14296
unknown <yunikashakya082@gmail.com>
2023-08-22 16:30:31 +0545

    updated viewproducts page using react-table pagination

--- src/Components/Products/ViewProducts.tsx
+++ src/Components/Products/ViewProducts.tsx
@@ -1,4 +1,4 @@
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
 import axios from "axios";
 import { useState } from "react";
 import { useQuery } from "react-query";
@@ -57,6 +57,7 @@ export const ViewProducts = () => {
         data: productList,
         columns: productColumns,
         getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
     });
 
     const { isLoading, error } = useQuery(["repo"], () =>
@@ -108,6 +109,60 @@ export const ViewProducts = () => {
                                 ))}
                             </tbody>
                         </table>
+<br />
+                        <div className="flex items-center gap-2">
+                            <button className="border rounded p-1"
+                                onClick={() => table.setPageIndex(0)}
+                                disabled={!table.getCanPreviousPage()}>
+                                {'<<'}
+                            </button>
+                            <button className="border rounded p-1"
+                                onClick={() => table.previousPage()}
+                                disabled={!table.getCanPreviousPage()}>
+                                {'<'}
+                            </button>
+                            <button className="border rounded p-1"
+                                onClick={() => table.nextPage()}
+                                disabled={!table.getCanNextPage()}>
+                                {'>'}
+                            </button>
+                            <button className="border rounded p-1"
+                                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                                disabled={!table.getCanNextPage()}>
+                                {'>>'}
+                            </button>
+                            <span className="flex items-center gap-1">
+                                <div>Page</div>
+                                <strong>
+                                    {table.getState().pagination.pageIndex + 1} of{' '}
+                                    {table.getPageCount()}
+                                </strong>
+                            </span>
+                            <span className="flex items-center gap-1">
+                                | Go to page:
+                                <input
+                                    type="number"
+                                    defaultValue={table.getState().pagination.pageIndex + 1}
+                                    onChange={e => {
+                                        const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                        table.setPageIndex(page)
+                                    }}
+                                    className="border p-1 rounded w-16"
+                                />
+                            </span>
+                            <select
+                                value={table.getState().pagination.pageSize}
+                                onChange={e => {
+                                    table.setPageSize(Number(e.target.value))
+                                }}
+                            >
+                                {[5,10, 20, 30, 40, 50].map(pageSize => (
+                                    <option key={pageSize} value={pageSize}>
+                                        Show {pageSize}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
